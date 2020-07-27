317abc083b0f995b11894f38a8c087485015fc58
Qais Ammari <ammariqais66@gmail.com>
2020-07-28 01:38:57 +0300

    finished animations, animations online, bullet follow mouse

--- Assets/Scripts/MovementTest.cs
+++ Assets/Scripts/MovementTest.cs
@@ -4,49 +4,52 @@ using Photon.Pun;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class MovementTest : MonoBehaviour {
+public class MovementTest : MonoBehaviourPun {
     public float moveSpeed = 0.5f;
     public Camera mainCamera;
     public Rigidbody2D rb;
-    public Weapon weapon;
     [SerializeField] private PhotonView _photonView;
     [SerializeField] private Animator _animator;
 
-    private Vector2 moveDirection, mousePosition,aimDirection;
+    private Vector2 moveDirection, mousePosition,movement;
 
     void Awake() {
       mainCamera = Camera.main;
     }
+    
+       /* void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.isWriting)
+        {
+            stream.SendNext(x);
+            stream.SendNext(y);
+            stream.SendNext(z);
+        }
+        else
+        {
+            x = (int)stream.ReceiveNext();
+            y = (int)stream.ReceiveNext();
+            z = (int)stream.ReceiveNext();
+        }
+    }
+    */
     void Update() {
-        float moveX = Input.GetAxis("Horizontal");
-        float moveY = Input.GetAxis("Vertical");
-        
-        moveDirection = new Vector2(moveX, moveY).normalized;
-        mousePosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        if (_photonView.IsMine) {
+            movement.x = Input.GetAxis("Horizontal");
+            movement.y = Input.GetAxis("Vertical");
+            moveDirection = new Vector2(movement.x, movement.y).normalized;
+            mousePosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 direction = (mousePosition - (Vector2) transform.position).normalized;
+            _animator.SetFloat("Horizontal",direction.x);
+            _animator.SetFloat("Vertical",direction.y);
+            _animator.SetFloat("Speed",movement.sqrMagnitude);
+        }
     }
     
     void FixedUpdate() {
       if (_photonView.IsMine) {
         float newYVelocity = moveDirection.y * moveSpeed;
         rb.velocity = new Vector2(moveDirection.x * moveSpeed, newYVelocity);
-        aimDirection = mousePosition - rb.position;
-        float aimAngle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg - 90f;
-       // rb.rotation = aimAngle;
       }
- 
-     // _animator.SetFloat("Walk",moveDirection.y * moveSpeed);
-     //_animator.SetFloat("SideWalk",moveDirection.x * moveSpeed);
-     if ((moveDirection.x * moveSpeed != 0) || (moveDirection.y * moveSpeed != 0)) {
-       _animator.SetFloat("SideWalk",aimDirection.x);
-       _animator.SetFloat("Walk",aimDirection.y);
-     } else {
-       _animator.SetFloat("SideWalk",0);
-       _animator.SetFloat("Walk",0);
-       Debug.Log("Aim : " + aimDirection.y);
-       _animator.SetFloat("Aim",aimDirection.y);
-       Debug.Log("SideAim : "+ aimDirection.x);
-       _animator.SetFloat("SideAim",aimDirection.x);
-     }
-
     }
 }
