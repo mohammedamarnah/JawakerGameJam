87c8e1fbceacbc1b7055a3b39cbae81fe16e6b5f
Mohammed Amarnah <m.amarnah@gmail.com>
2020-07-28 15:50:46 +0300

    weird stuff

---
+++ Assets/MenuController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour {
+    [SerializeField]
+    Button play, sounds;
+    [SerializeField]
+    Text soundsText;
+
+    void Start() {
+        play.onClick.AddListener(Play);
+        sounds.onClick.AddListener(ToggleSound);
+    }
+
+    void Play() {
+        this.gameObject.SetActive(false);
+    }
+
+    void ToggleSound() {
+        int audioLevel = 0;
+        soundsText.text = $"sounds: {(audioLevel > 0 ? "on" : "off")}";
+    }
+}
